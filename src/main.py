6277f08c9cb7b20095102c5ec51c261f064b422a
opavliuk <opavliuk@llnw.com>
2020-10-07 14:35:43 +0300

    Add dictionary CLI args

--- src/main.py
+++ src/main.py
@@ -5,14 +5,24 @@ from src.board import make_board
 BOARD_SIZE = 15
 
 
+class ArgumentError(Exception):
+    pass
+
+
 def _validate_args(args):
     if args.board_size < 0:
         raise ValueError('Board size should be > 0')
+    if args.dictionary_file and args.words:
+        raise ArgumentError('Please provide only one argument with words to search for'
+                            '(`--dictionary_file or --words`).')
 
 
 def _parse_cl_args():
     parser = argparse.ArgumentParser(description='Find words in two-dimensional array of random letters.')
     parser.add_argument('--board_size', type=int, default=BOARD_SIZE, help='Board side size')
+    parser.add_argument('--dictionary_file', type=str, help='File with words, separated by newline.'
+                                                            'Excludes `--words` argument if provided.')
+    parser.add_argument('--words', nargs='+', help='Words to search for, comma-separated.')
     args = parser.parse_args()
     _validate_args(args)
     return args
