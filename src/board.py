b1a97792cbb61408687f5fbc500f43a8023ce610
opavliuk <opavliuk@llnw.com>
2020-10-07 12:56:40 +0300

    Add assertion message

--- src/board.py
+++ src/board.py
@@ -3,5 +3,5 @@ import string
 
 
 def make_board(size):
-    assert size > 0
+    assert size > 0, 'Board size should be > 0.'
     return [random.sample(string.ascii_lowercase, size) for i in range(size)]
