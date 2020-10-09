4330858881dc7c7db604c839f804b116e48e7501
opavliuk <opavliuk@llnw.com>
2020-10-09 20:43:43 +0300

    Refactor naive search

--- src/search_engines/naive_search.py
+++ src/search_engines/naive_search.py
@@ -1,40 +1,41 @@
+import functools
 from multiprocessing import Pool
 
 
-def fws(i, j):
+def _forwards(i, j):
     return i, j + 1
 
 
-def bws(i, j):
+def _backwards(i, j):
     new_j = j - 1
     if new_j > 0:
         return i, new_j
     raise IndexError
 
 
-def upws(i, j):
+def _upwards(i, j):
     new_i = i - 1
     if new_i > 0:
         return new_i, j
     raise IndexError
 
 
-def dws(i, j):
+def _downwards(i, j):
     return i + 1, j
 
 
-def fuws(i, j):
+def _forward_upwards(i, j):
     new_i = i - 1
     if new_i > 0:
         return new_i, j + 1
     raise IndexError
 
 
-def fdws(i, j):
+def _forward_downwards(i, j):
     return i + 1, j + 1
 
 
-def buws(i, j):
+def _back_upwards(i, j):
     new_i = i - 1
     new_j = j - 1
     if new_i > 0 and new_j > 0:
@@ -42,17 +43,19 @@ def buws(i, j):
     raise IndexError
 
 
-def bdws(i, j):
+def _back_downwards(i, j):
     new_j = j - 1
     if new_j > 0:
         return i + 1, new_j
     raise IndexError
 
 
-directions = (fws, bws, upws, dws, fuws, fdws, buws, bdws)
+directions = (_forwards, _backwards, _upwards, _downwards,
+              _forward_upwards, _forward_downwards, _back_upwards, _back_downwards)
 
 
-def track_word(n, word, i, j, board, direction, found_word=''):
+def track_word(n, word, i, j, board, direction):
+    found_word = ''
     if word[n] == board[i][j]:
         found_word += word[n]
         if n == len(word) - 1:
@@ -63,7 +66,7 @@ def track_word(n, word, i, j, board, direction, found_word=''):
         raise StopIteration
 
 
-def iterate_board(board, word):
+def _iterate_board(board, word):
     for i, row in enumerate(board):
         for j, char in enumerate(row):
             for direction in directions:
@@ -76,13 +79,6 @@ def iterate_board(board, word):
 
 
 def naive_search(words, board):
-    import functools
     with Pool() as p_pool:
-        results = p_pool.map(functools.partial(iterate_board, board), words)
-
-    # results = []
-    # for word in words:
-    #     word_found = iterate_board(board, word)
-    #     if word_found:
-    #         results.append(word_found)
+        results = p_pool.map(functools.partial(_iterate_board, board), words)
     return [word for word in results if word]
