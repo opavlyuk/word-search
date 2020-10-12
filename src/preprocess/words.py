ce2197928cd77e5cc389638fdb9896e758a77fa4
opavliuk <opavliuk@llnw.com>
2020-10-12 17:06:31 +0300

    Add docstrings

--- src/preprocess/words.py
+++ src/preprocess/words.py
@@ -2,6 +2,15 @@ from src.helpers.file_dict_source import get_words_from_file
 
 
 def preprocess_words(args):
+    """Prepare input for convenient processing.
+
+    Args:
+        args: object with `words` or `dictionary_file` attrs.
+
+    Returns:
+        set, int, int: Set of words to search for, minimal word length, maximal word length
+
+    """
     words = set()
     if args.words:
         min_len, max_len = min(args.words, key=len), max(words, key=len)
