b5325cd56724abc04d351108149e2404456cd0bf
opavliuk <opavliuk@llnw.com>
2020-10-12 15:48:37 +0300

    Refactor to simplify testing

--- src/main.py
+++ src/main.py
@@ -4,11 +4,14 @@ from src.preprocess.words import preprocess_words
 from src.search_engines.advanced_search import advanced_search as search
 
 
-def main():
-    args = parse_cl_args()
+def search_words(args):
     board = make_board(args.board_size)
     words, min_len, max_len = preprocess_words(args)
-    found_words = search(words, board, min_len, max_len)
+    return search(words, board, min_len, max_len)
+
+
+def main(args):
+    found_words = search_words(args)
     if found_words:
         print('Words found:', ', '.join(found_words))
     else:
@@ -16,4 +19,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main(parse_cl_args())
